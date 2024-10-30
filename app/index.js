d5f07f6289573924fdc2d6fff2526c1398ce1466
lucasoli13 <lucasolijr@gmail.com>
2024-10-30 11:04:10 +0100

    Version 9.7

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 function suma(a, b) {
-    return a + b;
-  }
-  
-  module.exports = suma;
-  
+  return a + b;
+}
+
+console.log("Iniciando el contenedor suma-container...");
+
+// Ejecuta la función cada minuto (60,000 ms)
+setInterval(() => {
+  const resultado = suma(2, 3);  // Puedes cambiar estos valores si necesitas otros números
+  console.log(`Resultado de suma(2, 3): ${resultado}`);
+}, 60000);
